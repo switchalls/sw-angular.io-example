d9e081a5f260ae35c3ed0856af7023e5be4e4e1e
stewart.witchalls <stewart.witchalls@cdl.co.uk>
2020-02-28 11:06:33 +0000

    Step 6c. Pass product details into onNotify() dialog

--- sw.angular.io-example.war/src/main/angular.io-example/src/app/product-list/product-list.component.ts
+++ sw.angular.io-example.war/src/main/angular.io-example/src/app/product-list/product-list.component.ts
@@ -15,9 +15,8 @@ export class ProductListComponent {
     window.alert(productName + ' has been shared!');
   }
   
-  
-  onNotify() {
-    window.alert('You will be notified when the product goes on sale');
+  onNotify(productName) {
+    window.alert('You will be notified when ' + productName + ' goes on sale');
   }
 
 }
