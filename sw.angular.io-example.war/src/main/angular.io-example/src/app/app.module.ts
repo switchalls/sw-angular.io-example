0096d5f7fc198c4a0426326b4ba0590388d730f4
stewart.witchalls <stewart.witchalls@cdl.co.uk>
2020-02-29 00:18:42 +0000

    Step 7b. Using route information

--- sw.angular.io-example.war/src/main/angular.io-example/src/app/app.module.ts
+++ sw.angular.io-example.war/src/main/angular.io-example/src/app/app.module.ts
@@ -22,7 +22,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   bootstrap: [ AppComponent ]
 })
--- sw.angular.io-example.war/src/main/angular.io-example/src/app/product-details/product-details.component.ts
+++ sw.angular.io-example.war/src/main/angular.io-example/src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
+  product;
+    
+  constructor(private route: ActivatedRoute) {
+    console.log(route.params);
+  }
+
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+    });
   }
 
 }
