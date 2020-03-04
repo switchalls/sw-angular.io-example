ce82b44e647367177186a06b253f781a753322cd
stewart.witchalls <stewart.witchalls@cdl.co.uk>
2020-03-04 08:07:05 +0000

    Step 8c. Retrieve shipping prices

--- sw.angular.io-example.war/src/main/angular.io-example/src/app/cart.service.ts
+++ sw.angular.io-example.war/src/main/angular.io-example/src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +7,9 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   items = [];
+
+  constructor(private http: HttpClient) {
+  }
 
   addToCart(product) {
     this.items.push(product);
@@ -18,6 +22,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 
 }
