6ef3118564c0521f96ca060c8c9defb5dd68057d
stewart.witchalls <stewart.witchalls@cdl.co.uk>
2020-03-04 07:41:57 +0000

    Step 8a. Create the shopping cart service

--- sw.angular.io-example.war/src/main/angular.io-example/src/app/product-details/product-details.component.ts
+++ sw.angular.io-example.war/src/main/angular.io-example/src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +13,10 @@ export class ProductDetailsComponent implements OnInit {
 
   product;
     
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService,) {
+
     console.log(route.params);
   }
 
@@ -20,6 +24,11 @@ export class ProductDetailsComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
     });
+  }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 
 }
